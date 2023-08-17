949815278ee330e29da6a367f704177500280cc2
Weerawut1996 <wirawoot789@hotmail.com>
2023-08-17 16:21:32 +0700

    add connect ico

---
+++ src/assets/pubilc_ico.jsx
@@ -0,0 +1,6 @@
+import iconHome  from './icon/home_icon.svg'
+import iconP1 from './icon/one_1_icon.svg'
+import iconP2 from './icon/two_2_icon.svg'
+import iconP3 from './icon/three_3_icon.svg'
+ const iconpic = [ iconHome, iconP1,iconP2,iconP3 ]
+export default iconpic
