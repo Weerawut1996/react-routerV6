3a6740350780e8bf6dee78e359e4ea9ed19c6db6
Weerawut1996 <wirawoot789@hotmail.com>
2023-08-17 16:24:34 +0700

    add class nav

--- src/assets/Component/NavberTab.jsx
+++ src/assets/Component/NavberTab.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom"
 function NavberTab() {
    return (
       <>
-         <nav id = "nanvar">
-            <div className="d-inline-block">icon</div>
+         <nav id = "nanvar" >
+            <div className="d-inline-block text-fff">icon</div>
             <div className="d-flex gap-10px">
                <Link to={'/'}>Home</Link>
                <Link to={'/1stPage'}>1st Page</Link>
