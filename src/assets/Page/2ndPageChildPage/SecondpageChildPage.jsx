664323b09729e2df23d5d14f154976f870ce2b41
Weerawut1996 <wirawoot789@hotmail.com>
2023-08-17 16:25:07 +0700

    add secondpage component

---
+++ src/assets/Page/2ndPageChildPage/SecondpageChildPage.jsx
@@ -0,0 +1,55 @@
+function SecondpageChild1st() {
+   return (
+      <>
+         <h3>Second - page - Child - 1st</h3>
+         <h3>11111111111111111</h3>
+         <div>
+            <p>Export multiple functions or components from a file in React </p>
+         </div>
+      </>
+   )
+}
+
+function SecondpageChild2st() {
+   return (
+      <>
+         <h3>Second - page - Child - 2nd</h3>
+         <h3>22222222222222222</h3>
+         <div>
+            <p>Export multiple functions or components from a file in React </p>
+            <p>Export multiple functions or components from a file in React </p>
+         </div>
+      </>
+   )
+}
+
+const SecondpageChild3st = () => {
+   return (
+      <>
+         <h3>Second - page - Child - 3nd</h3>
+         <h3>33333333333333333</h3>
+         <div>
+            <p>Export multiple functions or components from a file in React </p>
+            <p>Export multiple functions or components from a file in React </p>
+            <p>Export multiple functions or components from a file in React </p>
+         </div>
+      </>
+   )
+}
+
+function SecondpageChild4th() {
+   return (
+      <>
+         <h3>Second - page - Child - 4th</h3>
+         <h3>44444444444444444</h3>
+         <div>
+            <p>Export multiple functions or components from a file in React </p>
+            <p>Export multiple functions or components from a file in React </p>
+            <p>Export multiple functions or components from a file in React </p>
+            <p>Export multiple functions or components from a file in React </p>
+         </div>
+      </>
+   )
+}
+
+export { SecondpageChild1st, SecondpageChild2st, SecondpageChild3st, SecondpageChild4th }
