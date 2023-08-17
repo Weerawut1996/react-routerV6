df3e11caf427d582ab5ef18a76ad6712adfe5f02
Weerawut1996 <wirawoot789@hotmail.com>
2023-08-17 16:22:52 +0700

    add Router component

--- src/assets/Router.jsx
+++ src/assets/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Firstpage from './Page/FirstPage'
-import {FirstpageChild1st ,FirstpageChild2st,FirstpageChild3st, FirstpageChild4th } from './Page/1stPageChildPage/FirstpageChildPage'
+import { FirstpageChild1st, FirstpageChild2st, FirstpageChild3st, FirstpageChild4th } from './Page/1stPageChildPage/FirstpageChildPage'
+import { SecondpageChild1st, SecondpageChild2st, SecondpageChild3st, SecondpageChild4th } from './Page/2ndPageChildPage/secondpageChildPage';
 import SecondPage from './Page/SecondPage';
 import ThirdPage from './Page/ThirdPage';
 import ErrorPage from './Page/ErrorPage';
@@ -22,12 +23,18 @@ const router = createBrowserRouter([
          { path: "/1stPage/ChildPage2", element: <FirstpageChild2st /> },
          { path: "/1stPage/ChildPage3", element: <FirstpageChild3st /> },
          { path: "/1stPage/ChildPage4", element: <FirstpageChild4th /> },
-       ],
+      ],
    },
    {
       path: "/2ndPage",
       element: <SecondPage />,
       errorElement: <ErrorPage />,
+      children: [
+         { path: "/2ndPage/ChildPage1", element: <SecondpageChild1st /> },
+         { path: "/2ndPage/ChildPage2", element: <SecondpageChild2st /> },
+         { path: "/2ndPage/ChildPage3", element: <SecondpageChild3st /> },
+         { path: "/2ndPage/ChildPage4", element: <SecondpageChild4th /> },
+      ],
    },
    {
       path: "/3rdPage",
