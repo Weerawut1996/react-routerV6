8bdb76937997a0d28a7bc5738515d4291c4acc5b
Weerawut1996 <wirawoot789@hotmail.com>
2023-08-18 13:42:14 +0700

    add css flie

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import './idCss.css'
+import './classCss.css'
 import router from './assets/Router';
 import { RouterProvider } from 'react-router-dom'
 import PageFooter from './assets/Component/PageFooter';
