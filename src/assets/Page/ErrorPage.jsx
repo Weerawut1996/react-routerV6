f15c360fc6b3c66b0c934cf95a3e9408833fd96c
Weerawut1996 <wirawoot789@hotmail.com>
2023-08-18 11:07:41 +0700

    edit error page

--- src/assets/Page/ErrorPage.jsx
+++ src/assets/Page/ErrorPage.jsx
@@ -1,10 +1,12 @@
+import iconpic from "../pubilc_ico";
+
 function ErrorPage() {
    document.title = 'Error 404';
+   document.getElementById('root').style.display = 'block';
+   document.getElementById('page-ico').setAttribute('href',iconpic[0]);
    return (
      <>
-       <h3>404</h3>    
-       <div>
-       </div>
+       <h1 style={{textAlign:"center"}}>404 Not found page</h1>
      </>
    )
  }
