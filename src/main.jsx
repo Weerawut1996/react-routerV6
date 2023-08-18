4c0d28c7701855a97019fd6407df157f9ad651e6
Weerawut1996 <wirawoot789@hotmail.com>
2023-08-18 11:07:13 +0700

    add PageFooter

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import './idCss.css'
 import router from './assets/Router';
 import { RouterProvider } from 'react-router-dom'
+import PageFooter from './assets/Component/PageFooter';
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
      <RouterProvider router={router} />
+     <PageFooter/>
    </React.StrictMode>
  );
